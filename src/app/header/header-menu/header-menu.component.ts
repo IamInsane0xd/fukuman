5ecb4f25a38a59ecf2285ac6785508263cd8684e
biczoferenc <biczo.ferenc@icloud.com>
2023-11-24 11:26:51 +0100

    add basic header

---
+++ src/app/header/header-menu/header-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header-menu.component.html',
+  styleUrl: './header-menu.component.less'
+})
+export class HeaderMenuComponent {
+
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from "@angular/common";
+import {HeaderMenuComponent} from "./header-menu/header-menu.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage, HeaderMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
 export class HeaderComponent {
-
+  logoUrl: string = "/assets/logo/logo-transparent.png";
 }
