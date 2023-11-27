6c8c32dbe984609642886e4223c76230077200ac
biczoferenc <biczo.ferenc@icloud.com>
2023-11-27 14:12:28 +0100

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {ContentComponent} from "./content/content.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, ContentComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
